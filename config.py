08cf386364353a14d8b326576ee811017999aa18
Muhammed El Omer <67698326+mhmad240@users.noreply.github.com>
2026-06-13 05:28:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # D:\bot_telegram\config.py
 import os
-TOKEN = os.environ.get("TOKEN", "8875853980:AAFfNyVULQWO59fyqyVLTBdVfC10-VgdlSc")
+TOKEN = os.environ.get("TOKEN", "ضع_التوكن_هنا_للاستخدام_المحلي")
